cf92a5c7ff473e46520605c01e78b5c5917fadc5
wlsp <wlsp@outlook.de>
2021-05-19 23:26:14 +0200

    random-math-quizzer

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+
+var randomOne = parseInt(document.getElementById('random01'))
+var randomTwo = parseInt(document.getElementById('random02'))
+
+function newRatsel() {
+    randomOne = parseInt(random01.innerText = Math.floor(Math.random() * (100 - 0)) + 0);
+    randomTwo = parseInt(random02.innerText = Math.floor(Math.random() * (100 - 0)) + 0);
+    resultIn = randomOne + randomTwo
+    answer.innerHTML = " "
+    wrongAnswer.innerHTML = " "
+    return resultIn
+}
+
+var resultIn = newRatsel();
+console.log(resultIn)
+
+function resultIs() {
+    var resulto = parseInt(document.getElementById('result').value)
+    console.log(resulto);
+    if (resulto === resultIn) {
+        answer.classList.add('correct')
+        answer.innerHTML = "Very noice! <br> *Borat voice*"
+        console.log('mmh!')
+        wrongAnswer.innerHTML = " "
+    }
+    else {
+        wrongAnswer.classList.add('wrong')
+        wrongAnswer.innerHTML = "Try again, buddy!"
+        console.log('Not correct!')
+    }
+}
+
